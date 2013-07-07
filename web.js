92602bfad868da863e265f57d65e9ba44f4e342b
Heath Matlock <heathmatlock@gmail.com>
2013-07-07 17:41:14 -0500

    modify web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var index = fs.readFileSync('index.html', 'utf-8', function(
+var cachedPage = fs.readFileSync('index.html', 'utf-8', function(err, data) {
+  if (err) console.log('err: ', err);//TODO: throw it in a logfile
+  return data;
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(cachedPage);
 });
 
 var port = process.env.PORT || 5000;
